ef2f0558b815bdce042c5fd64ab7131d06f7709c
SammySoap <samuelerodighiero@gmail.com>
2024-06-21 23:57:37 +0200

    Created the server that will shorten the download url

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.redirect(`https://raw.githubusercontent.com/SammySoap/dwm/main/setup.sh`);
+});
+
+app.get('/:branch', function(req, res) {
+    const branch = req.params.branch;
+    console.log(branch);
+
+    res.redirect(`https://raw.githubusercontent.com/SammySoap/dwm/${branch}/setup.sh`);
+});
+
+app.listen(PORT, function() {
+    console.log(`[Server] -> Listening on port ${PORT}`);
+});
